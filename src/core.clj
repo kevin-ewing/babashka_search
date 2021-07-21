04ca2a38da94b6060bd91830f747989e88f8b7ac
Kevin Ewing <kevin.ewing@genesys.com>
2021-07-21 16:16:56 -0400

    Added time

--- src/core.clj
+++ src/core.clj
@@ -1,5 +1,12 @@
 (ns core
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io])
+  (:import [java.time]))
+
+(defn from-unix-time
+  "Return a Java Date object from a Unix time representation expressed
+  in whole seconds."
+  [unix-time]
+  (java.util.Date. unix-time))
 
 (defn- get-names
   [files]
@@ -33,15 +40,24 @@
           (get-size (rest files)))
     (list)))
 
+(defn- get-last
+  [files]
+  "Get paths from sequence"
+  (if (first files)
+    (cons (.lastModified (first files))
+          (get-last (rest files)))
+    (list)))
+
 (defn- make-map
   "Makes a map of all files in folder"
-  [seq-names seq-isdir seq-path seq-size]
+  [seq-names seq-isdir seq-path seq-size seq-last]
   (if (first seq-names)
     (cons (hash-map :name (first seq-names)
                     :is-dir (first seq-isdir)
                     :path (first seq-path)
-                    :size (first seq-size))
-          (make-map (rest seq-names) (rest seq-isdir) (rest seq-path) (rest seq-size)))
+                    :size (first seq-size)
+                    :last (first seq-last))
+          (make-map (rest seq-names) (rest seq-isdir) (rest seq-path) (rest seq-size) (rest seq-last)))
     (list)))
 
 
@@ -54,12 +70,14 @@
          seq-isdir (is-direct all-files)
          seq-path (get-path all-files)
          seq-size (get-size all-files)
-         file-map (make-map seq-names seq-isdir seq-path seq-size)
+         seq-last (get-last all-files)
+         file-map (make-map seq-names seq-isdir seq-path seq-size seq-last)
          name-matched (filter #(= (:name %) file) file-map)]
      (dotimes [x (count name-matched)]
        (println (:name (nth name-matched x)))
        (println (str "   Path: " (:path (nth name-matched x))))
-       (println (str "   Size: " (:size (nth name-matched x)) " bytes")))
+       (println (str "   Size: " (:size (nth name-matched x)) " bytes"))
+       (println (str "   Last Modified: " (from-unix-time (:last (nth name-matched x))))))
      (dotimes [x (count file-map)]
        (if (and (nth file-map x) (not= "Library" (:name (nth file-map x))))
          (-main start file (:path (nth file-map x))))))))
